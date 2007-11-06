c2a700a42f0543c619f101406e99c57ffb71cd5b
soiland <soiland@5797683a-9819-0410-b7be-41930ee49dfe>
2007-11-06 09:14:59 +0000

    Added serial version
    git-svn-id: https://perfs.googlecode.com/svn/java/trunk@66 5797683a-9819-0410-b7be-41930ee49dfe

--- api/src/main/java/com/googlecode/perfs/fs/AlreadyExistsException.java
+++ api/src/main/java/com/googlecode/perfs/fs/AlreadyExistsException.java
@@ -2,6 +2,7 @@ package com.googlecode.perfs.fs;
 
 public class AlreadyExistsException extends IllegalStateException {
 
+	private static final long serialVersionUID = -6188441877588121147L;
 	private final String filename;
 
 	public AlreadyExistsException(String filename) {
