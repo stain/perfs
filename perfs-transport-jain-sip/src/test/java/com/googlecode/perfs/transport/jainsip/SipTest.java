98b6e17487051e18a760f4cf2175ed3d5db8b4bb
Stian Soiland-Reyes <stian@soiland-reyes.com>
2010-06-05 22:33:41 +0100

    ignore test

--- perfs-transport-jain-sip/src/test/java/com/googlecode/perfs/transport/jainsip/SipTest.java
+++ perfs-transport-jain-sip/src/test/java/com/googlecode/perfs/transport/jainsip/SipTest.java
@@ -5,15 +5,16 @@ import static org.junit.Assert.*;
 import javax.sip.SipFactory;
 import javax.sip.address.TelURL;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SipTest {
 
-//	@Test
-//	public void hello() throws Exception {
-//		SipFactory factory = SipFactory.getInstance();
-//		TelURL createTelURL = factory.createAddressFactory().createTelURL("");
-//		factory.createSipStack(null).
-//		
-//		}
+	@Ignore
+	@Test
+	public void hello() throws Exception {
+		SipFactory factory = SipFactory.getInstance();
+		TelURL createTelURL = factory.createAddressFactory().createTelURL("");
+		factory.createSipStack(null);		
+	}
 }
